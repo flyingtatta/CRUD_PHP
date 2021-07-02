3aadb6e1304894516da3f8a4a1616476ff46a1e7
hamzajg16 <hamzajg16@gmail.com>
2021-07-03 00:49:29 +0530

    Created the header footer and index page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  include 'includes/db.php';
+  include 'includes/functions.php';
+  include 'includes/modules/header.php';
+  include 'includes/components/navigation.php';
+  include 'includes/components/view_items.php';
+  include 'includes/modules/footer.php';
+?>
