28ca2714cddec93cdb9bfb650ba02a906f4a4715
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2021-07-16 20:34:17 +0530

    added an icon to the titlel

--- includes/modules/header.php
+++ includes/modules/header.php
@@ -5,23 +5,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="icon" href="icon_path" type="images/desk.jpg">
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <style media="screen">
-    /* hide scrollbar but allow scrolling */
-    body {
-      -ms-overflow-style: none; /* for Internet Explorer, Edge */
-      scrollbar-width: none; /* for Firefox */
-      overflow-y: scroll;
-    }
+      /* hide scrollbar but allow scrolling */
+      body {
+        -ms-overflow-style: none; /* for Internet Explorer, Edge */
+        scrollbar-width: none; /* for Firefox */
+        overflow-y: scroll;
+      }
 
-    body::-webkit-scrollbar {
-      display: none; /* for Chrome, Safari, and Opera */
-    }
-
+      body::-webkit-scrollbar {
+        display: none; /* for Chrome, Safari, and Opera */
+      }
     </style>
   </head>
   <body>
