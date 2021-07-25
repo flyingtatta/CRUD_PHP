fbec323f0d24e545adbc738ea8214b6a7145938d
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2021-07-25 17:47:43 +0530

    Functionality to add products

--- includes/components/add_product.php
+++ includes/components/add_product.php
@@ -1,10 +1,105 @@
-<form class="mt-3" action="index.html" method="post">
+<?php
+
+  if (isset($_POST['submit_product'])){
+
+    $product_name  = escape($_POST['product_name']);
+    $product_price = escape($_POST['product_price']);
+    $product_desc  = escape($_POST['product_desc']);
+
+    foreach ($_FILES['image']['name'] as $key => $value) {
+      $product_image       = $_FILES['image']['name'][$key];
+      $tmp_product_image   = $_FILES['image']['tmp_name'][$key];
+      $directory = 'images/user_images/';
+      move_uploaded_file($tmp_product_image, $directory.$product_image);
+    }
+    $product_image = implode(', ',$_FILES['image']['name']);
+
+    $error = [
+      'product_name' => '',
+      'product_price' => '',
+      'product_desc' => ''
+    ];
+
+    if (empty($_POST['product_name'])){
+      $error['product_name'] = 'Name cannot be empty';
+    }
+
+    if (empty($_POST['product_desc'])){
+      $error['product_desc'] = 'Description cannot be empty';
+    }
+
+    if (ctype_space($_POST['product_desc'])){
+      $error['product_desc'] = 'Type something in it';
+    }
+
+    if (empty($_POST['product_price'])){
+      $error['product_price'] = 'Price cannot be empty';
+    }
+
+    if ($_POST['product_price'] < 0){
+      $error['product_price'] = 'Price cannot be negative';
+    }
+
+
+    foreach ($error as $key => $value) {
+      if(empty($value)){
+        unset($error[$key]);
+      }
+    }
+
+    if (empty($error)){
+      addProduct($product_name, $product_desc, $product_image, $tmp_product_image, $product_price);
+    }
+  }
+
+?>
+
+
+<form class="mt-3" action="" method="post"  enctype="multipart/form-data">
   <div class="form-row justify-content-center">
     <div class="form-group col-md-3">
-      <input type="email" class="form-control" placeholder="Name">
+      <input
+        type="text"
+        name="product_name"
+        placeholder="Name"
+        value = "<?php if (isset($_POST['submit_product'])): echo $_POST['product_name']; endif; ?>"
+        class="form-control
+        <?php if (isset($error['product_name'])){
+          echo "border-danger";
+        }
+        else {
+          echo "border-success";
+        }
+        ?>
+        "
+      >
+      <?php if (isset($error['product_name'])): ?>
+        <small class="small text-danger m-0 p-0">
+          <?php echo $error['product_name']; ?>
+        </small>
+      <?php endif; ?>
     </div>
     <div class="form-group col-md-3">
-      <input type="password" class="form-control" placeholder="Price">
+      <input
+        type="number"
+        name="product_price"
+        placeholder="Price"
+        value = "<?php if (isset($_POST['submit_product'])): echo $_POST['product_price']; endif; ?>"
+        class="form-control
+        <?php if (isset($error['product_price'])){
+          echo "border-danger";
+        }
+        else {
+          echo "border-success";
+        }
+        ?>
+        "
+      >
+      <?php if (isset($error['product_price'])): ?>
+        <small class="small text-danger m-0 p-0">
+          <?php echo $error['product_price']; ?>
+        </small>
+      <?php endif; ?>
     </div>
 
     <div class="form-group col-md-3">
@@ -19,14 +114,41 @@
 
   <div class="form-row justify-content-center">
     <div class="form-group col-md-8">
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
-      <small id="exampleFormControlTextarea1" class="form-text text-primary">
-        Give a small description about the thing that you'll be uploading
+      <textarea
+        class="form-control
+        <?php if (isset($error['product_desc'])){
+          echo "border-danger";
+        }
+        else {
+          echo "border-success";
+        }
+        ?>"
+        id="exampleFormControlTextarea1"
+        rows="1"
+        name="product_desc"></textarea>
+
+      <small
+        id="exampleFormControlTextarea1"
+        class="form-text
+        <?php if (isset($error['product_desc'])){
+          echo "text-danger";
+        }
+        else {
+          echo "text-primary";
+        }
+        ?>">
+
+        <?php if (isset($error['product_desc'])){
+          echo $error['product_desc'];
+        }
+        else{
+          echo "Give a small description about the thing that you'll be uploading";
+        } ?>
       </small>
     </div>
 
     <div class="form-group col-md-2">
-      <input type="submit" class="form-control btn btn-outline-primary" name="" value="Submit">
+      <input type="submit" class="form-control btn btn-outline-primary" name="submit_product" value="Submit">
     </div>
   </div>
 </form>
--- includes/functions.php
+++ includes/functions.php
@@ -2,21 +2,34 @@
 
 function confirmQuery($query){
   global $connection;
-
   if (!$query){
     die("Query Failed!! ".mysqli_error($connection));
   }
 }
 
+function escape($string){
+  global $connection;
+  return mysqli_real_escape_string($connection, $string);
+}
+
 
 function getProducts(){
-
   global $connection;
-
   $stmt = mysqli_query($connection, "SELECT * FROM products;");
   confirmQuery($stmt);
   return $stmt;
+}
 
+
+
+function addProduct($product_name, $product_desc, $product_image, $tmp_product_image, $product_price){
+  global $connection;
+
+  $stmt = mysqli_prepare($connection, "INSERT INTO products(product_name, product_desc, product_price, product_image) VALUES(?,?,?,?)");
+  mysqli_stmt_bind_param($stmt, 'ssis', $product_name, $product_desc, $product_price, $product_image);
+  mysqli_stmt_execute($stmt);
+  confirmQuery($stmt);
+  header("Location: ./index.php");
 }
 
 ?>
